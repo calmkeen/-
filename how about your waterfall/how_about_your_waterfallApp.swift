48038ee3ecabce34b3163606cb6848560545a23d
calmkeen <iyunseong159@gmail.com>
2022-04-08 15:52:33 +0900

    Initial Commit

---
+++ how about your waterfall/how_about_your_waterfallApp.swift
@@ -0,0 +1,17 @@
+//
+//  how_about_your_waterfallApp.swift
+//  how about your waterfall
+//
+//  Created by calmkeen on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct how_about_your_waterfallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
